575eb4d53a0adc33b99ab384dfdbb617b30c79fc
Guilherme <129104268+Guilherme0112@users.noreply.github.com>
2024-04-17 21:46:55 -0300

    Add files via upload

--- Adiministração de Produtos.py
+++ Adiministração de Produtos.py
@@ -12,64 +12,90 @@ while True:
         ''')
         nav = input('O que deseja fazer? ').strip() [0]
         if nav == '1':  #record the values ​​in the list
-            print('=-' * 20)
-            product = input('Digite o nome do produto: ')
-            amount = int(input('Digite a quantidade do produto: '))
-            products = list()
-            products.append(product)
-            products.append(amount)
-            carrinho.append(products)
-            print('=-' * 20)
-            print('Produto cadastrado com sucesso!')
-            print('=-' * 20)
+            try:
+                print('=-' * 20)
+                product = input('Digite o nome do produto: ')
+                amount = int(input('Digite a quantidade do produto: '))
+                products = list()
+                products.append(product)
+                products.append(amount)
+                carrinho.append(products)
+                print('=-' * 20)
+                print('Produto cadastrado com sucesso!')
+                print('=-' * 20)
+            except:
+                print('=-' * 20)
+                print('Algo deu errado! Tente novamente. :(')
+                print('=-' * 20)
         elif nav == '2': #shows each value in the list
             print('=-' * 20)
-            for sublist in enumerate(carrinho):
-                print(f'{sublist[0]} | {sublist[1]}')
+            for lista in enumerate(carrinho):
+                print(f'{lista[0]} | {lista[1]}')
             print('=-' * 20)
         elif nav == '3': #updating list values
-            print('=-' * 20)
-            print('''
-            [1] Vendeu 
-            [2] Comprou
-            [3] Voltar
-            ''')
-            print('=-' * 20)
-            update_list = input('Você vendeu produtos ou repôs o estoque? ')
-            print('=-' * 20)
-            if update_list == '1':
-                for sublist in enumerate(carrinho):
-                    print(f'{sublist[0]} | {sublist[1]}')
+            try:
                 print('=-' * 20)
-                update_id = int(input('Qual o id produto? '))
-                update_amount = int(input(f'Digite o total de produtos vendidos do {carrinho[update_id]}: '))
-            elif update_list == '2':
-                print('comprou')
+                print('''
+                [1] Vendeu 
+                [2] Comprou
+                [3] Voltar
+                ''')
                 print('=-' * 20)
-            elif update_list == '3':
-                print()
+                update_list = input('Você vendeu produtos ou repôs o estoque? ')
+                print('=-' * 20)
+                if update_list == '1': #sell products
+                    for sublist in enumerate(carrinho):
+                        print(f'{sublist[0]} | {sublist[1]}')
+                    print('=-' * 20)
+                    update_id = int(input('Qual o id produto? '))
+                    update_amount = int(input(f'Digite o total de produtos vendidos do {carrinho[update_id][0]}: '))
+                    if update_amount > carrinho[update_id][1]:
+                        print('O valor informado é maior que a quantidade de produtos!')
+                    else:
+                        update =  carrinho[update_id][1] - update_amount
+                        carrinho[update_id][1] = update
+                elif update_list == '2': #reset products
+                    for sublist in enumerate(carrinho):
+                        print(f'{sublist[0]} | {sublist[1]}')
+                    print('=-' * 20)
+                    update_id = int(input('Qual o id produto? '))
+                    update_amount = int(input(f'Digite o total de produtos que você repôs do {carrinho[update_id][0]}: '))
+                    update =  carrinho[update_id][1] + update_amount
+                    carrinho[update_id][1] = update
+                    print('=-' * 20)
+            except:
+                print('=-' * 20)
+                print('Valor inválido! Verifique o valor e tente novamente.')
+                print('=-' * 20)
             else: 
                 print('=-' * 20)
                 print('Valor Inválido!')
                 print('=-' * 20)
         elif nav == '4': #removing value from list
-            print('=-' * 20)
-            for sublist in enumerate(carrinho):
-                print(f'{sublist[0]} | {sublist[1]}')
-            print('=-' * 20)
-            remove = int(input('Qual produto deseja remover da lista? '))
-            print('=-' * 20)
-            remove_warn = input(f'Deseja remover o produto {carrinho[remove]}? [S/N] ').upper().strip() [0]
-            if remove_warn == 'S':
-                carrinho.pop(remove)
-                print('Produto removido com sucesso!')
+            try:
                 print('=-' * 20)
-            elif remove_warn == 'N':
+                for sublist in enumerate(carrinho):
+                    print(f'{sublist[0]} | {sublist[1]}')
                 print('=-' * 20)
-                print('Você cancelou a exclusão.')
+                remove = int(input('Qual produto deseja remover da lista? '))
                 print('=-' * 20)
-            elif remove_warn != 'S' and 'N':
-                print('Valor inválido!')
+                remove_warn = input(f'Deseja remover o produto {carrinho[remove]}? [S/N] ').upper().strip() [0]
+                if remove_warn == 'S':
+                    carrinho.pop(remove)
+                    print('Produto removido com sucesso!')
+                    print('=-' * 20)
+                elif remove_warn == 'N':
+                    print('=-' * 20)
+                    print('Você cancelou a exclusão.')
+                    print('=-' * 20)
+                elif remove_warn != 'S' and 'N':
+                    print('=-' * 20)
+                    print('Valor inválido!')
+                    print('=-' * 20)
+            except:
+                print('=-' * 20)
+                print('Valor inválido! Verifique os dados e tente novamente.')
+                print('=-' * 20)
         elif nav == '5': #end of program
             print('=-' * 20)
             print('Programa encerrado!')
